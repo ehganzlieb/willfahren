462ec1b7ad0ed77b7d8c48d0f38dfcb465be7191
ncl <ehganzlieb@proton.me>
2025-12-08 18:01:22 +0100

    finish lines parser (without stops)

--- src/wlClient/linesParser.go
+++ src/wlClient/linesParser.go
@@ -2,9 +2,25 @@ package wlclient
 
 import (
 	"encoding/csv"
+	"io"
+	"log"
 	"strings"
 
 	"github.com/ehganzlieb/willwohnen/src/dto"
+)
+
+const (
+	LineNameField = "BEZEICHNUNG"
+	LineTypeField = "VERKEHRSMITTEL"
+
+	TramString           = "ptTram"
+	UBahnString          = "ptMetro"
+	SBahnString          = "ptTrainS"
+	BusString            = "ptBusCity"
+	NightBusString       = "ptBusNight"
+	GroupTaxiString      = "pt_RufbusTag"
+	BadnerBahnString     = "ptBadner_Bahn"
+	NightGroupTaxiString = "Pt_RufbusNacht"
 )
 
 func parseLinesCSV(input string) ([]dto.Line, error) {
@@ -23,5 +39,40 @@ func parseLinesCSV(input string) ([]dto.Line, error) {
 	}
 
 	lines := make([]dto.Line, 0)
+
+	//parse all lines
+	for {
+		record, err := r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Print(err)
+			}
+		}
+		l := dto.Line{
+			Name: record[indexMap[LineNameField]],
+		}
+		switch record[indexMap[LineTypeField]] {
+		case TramString:
+			l.Type = dto.LineTypeTram
+		case UBahnString:
+			l.Type = dto.LineTypeUBahn
+		case SBahnString:
+			l.Type = dto.LineTypeSBahn
+		case BusString:
+			l.Type = dto.LineTypeBus
+		case NightBusString:
+			l.Type = dto.LineTypeNightBus
+		case GroupTaxiString:
+			l.Type = dto.LineTypeGroupTaxi
+		case BadnerBahnString:
+			l.Type = dto.LineTypeBadnerBahn
+		case NightGroupTaxiString:
+			l.Type = dto.LineTypeNightGroupTaxi
+		}
+		lines = append(lines, l)
+	}
+	return lines, nil
 
 }
