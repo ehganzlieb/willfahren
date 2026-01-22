c07bdda300c4fb51843529c2ea2683ba2179fcd8
ncl <ehganzlieb@proton.me>
2026-01-22 20:47:17 +0100

    update Distance() call in domain/lineFilter.go

--- domain/lineFilter.go
+++ domain/lineFilter.go
@@ -7,7 +7,7 @@ import (
 func FilterStops(stops []dto.Stop, maxdistance float64) ImmoListingsFilter {
 	return func(il ImmoListing) bool {
 		for _, stop := range stops {
-			if stop.Location.Distance(il.Location) <= maxdistance {
+			if stop.Location.Distance(il.Location, dto.DistanceFormulaDefault) <= maxdistance { //TODO: parametrize
 				return true
 			}
 		}
